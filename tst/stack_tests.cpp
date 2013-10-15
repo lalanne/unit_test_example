4b9cb352f70f9a93babb4fbdff220f34bab67303
Christian Lalanne <christian.lalanne@ichec.ie>
2013-10-15 12:38:28 +0100

    tests for stack class

---
+++ tst/stack_tests.cpp
@@ -0,0 +1,28 @@
+
+#include "Stack.hpp"
+
+#include <iostream>
+
+#include <gtest/gtest.h>
+
+using namespace std;
+
+TEST(stack_tests, lifo_behaviour_with_1_element){
+    Stack<float> stack;
+
+    stack.push(2.5);
+    
+    EXPECT_EQ(2.5, stack.pop());
+}
+
+TEST(stack_tests, lifo_behaviour_with_3_elements){
+    Stack<int> stack;
+
+    stack.push(2);
+    stack.push(1);
+    stack.push(14);
+    
+    EXPECT_EQ(14, stack.pop());
+    EXPECT_EQ(1, stack.pop());
+    EXPECT_EQ(2, stack.pop());
+}
